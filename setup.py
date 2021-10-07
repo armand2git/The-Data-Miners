b33b9019c0f1caad05e9b22155d4fb2b28d32c16
armand <armand.alarcon@estudiantat.upc.edu>
2021-10-07 19:46:42 +0200

    1st delivery

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='LAB01 from TAED2',
+    author='The Data Miners',
+    license='MIT',
+)
